d66dd2d88fe65a5f9e2a80b36f218702c9fb71fb
Selcuk <devselcukdev@gmail.com>
2022-12-25 00:47:03 +0100

    Initial Commit

---
+++ CD-Training/CD_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CD_TrainingApp.swift
+//  CD-Training
+//
+//  Created by SELCUK YILDIZ on 25.12.22.
+//
+
+import SwiftUI
+
+@main
+struct CD_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
